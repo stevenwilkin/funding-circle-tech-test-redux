ada36c163a014420b83c6311a4e6ccf857e7f1a4
Steven Wilkin <stevenwilkin@gmail.com>
2017-04-25 17:36:34 +0100

    Specify num of primes to generate on commandline

--- lib/primes_tables.rb
+++ lib/primes_tables.rb
@@ -1,6 +1,6 @@
 module PrimesTables
   def self.run!
-    primes = Util.generate_primes(10)
+    primes = Util.generate_primes(number_of_primes)
     table = Table.new(primes)
     display table.to_s
   end
@@ -8,7 +8,20 @@ module PrimesTables
   def self.display(string)
     puts string
   end
+
+  def self.number_of_primes
+    num_primes = 10
+
+    OptionParser.new do |opts|
+      opts.on('--count X', Integer) do |c|
+        num_primes = c if c > 0
+      end
+    end.parse!
+
+    num_primes
+  end
 end
 
+require 'optparse'
 require 'primes_tables/util'
 require 'primes_tables/table'
--- spec/primes_table_spec.rb
+++ spec/primes_table_spec.rb
@@ -2,10 +2,45 @@ RSpec.describe PrimesTables do
   let(:primes) { Array.new }
   let(:table) { double(:to_s => output) }
   let(:output) { String.new }
+  let(:num_primes) { 6 }
+  let(:default_num_primes) { 10 }
+
+  describe '.number_of_primes' do
+    context 'when the argument is not provided' do
+      before do
+        ARGV.clear
+      end
+
+      it 'uses a default' do
+        expect(described_class.number_of_primes).to eq(default_num_primes)
+      end
+    end
+
+    context 'when the argument is provided' do
+      before do
+        ARGV.replace(['--count', "#{num_primes}"])
+      end
+
+      context 'but is not a natural number' do
+        let(:num_primes) { 0 }
+
+        it 'uses a default' do
+          expect(described_class.number_of_primes).to eq(default_num_primes)
+        end
+      end
+
+      context 'and is a natural number' do
+        it 'uses the supplied value' do
+          expect(described_class.number_of_primes).to eq(num_primes)
+        end
+      end
+    end
+  end
 
   describe '.run!' do
     it 'sends the correct messages' do
-      expect(PrimesTables::Util).to receive(:generate_primes).with(10).and_return(primes)
+      expect(described_class).to receive(:number_of_primes).and_return(num_primes)
+      expect(PrimesTables::Util).to receive(:generate_primes).with(num_primes).and_return(primes)
       expect(PrimesTables::Table).to receive(:new).with(primes).and_return(table)
       expect(described_class).to receive(:display).with(output)
       described_class.run!
